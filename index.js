7dada3169eb381d54d583df5af85905533ed8fc6
caesar003 <caesarmuksid@gmail.com>
2020-04-21 23:44:15 +0800

    removing unnecessary files

--- index.js
+++ index.js
@@ -3,55 +3,55 @@ $(document).ready(function(){
 
   const images_links = [
     {
-      image : "single_carousel.png",
+      image : "single_carousel.jpg",
       title : "Single Page Carousel",
       link : "single_carousel",
       description : "Single page website which becomes more popular among modern web applications",
     },
     {
-      image : "strapped.png",
+      image : "strapped.jpg",
       title : "Strapped",
       link : "strapped",
       description : "Multi pages profile website is good option once you need to separate your content into different pages.",
     },
     {
-      image : "pattern.png",
+      image : "pattern.jpg",
       title : "Pattern",
       link : "pattern",
       description : "An image gallery accompanied with catchy caption and description. ",
     },
     {
-      image : "museum_of_candy.png",
+      image : "museum_of_candy.jpg",
       title : "Museum of Candy",
       link : "museum_of_candy",
       description : "Single page two-column website, for those who need several paragraphs but still want to images to stand out.",
     },
     {
-      image : "landing_page1.png",
+      image : "landing_page1.jpg",
       title : "Landing Page",
       link : "landing_page",
       description : "The first impression you want visitors to see once they come accross your site.",
     },
     {
-      image : "landing_page2.png",
+      image : "landing_page2.jpg",
       title : "Landing Page",
       link : "landing_page_2",
       description : "Another landing page",
     },
     {
-      image : "image_gallery.png",
+      image : "image_gallery.jpg",
       title : "Image Gallery",
       link : "image_gallery",
       description : "A perfect choice to display collection of your photos.",
     },
     {
-      image : "carousel.png",
+      image : "carousel.jpg",
       title : "Carousel",
       link : "carousel",
       description : "Provide a compact view for bunch of your product in a single page. ",
     },
     {
-      image: "blog.png",
+      image: "blog.jpg",
       title : "Blog post",
       link : "blog",
       description : "Simple blog post which worth considering for those who like writing and sharing ideas about particular subject such as food and travelling."
