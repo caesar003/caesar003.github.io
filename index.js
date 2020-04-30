9a389a29c697af37a2c89d6ccf98e783da8d702d
caesar003 <caesarmuksid@gmail.com>
2020-04-30 14:56:31 +0800

    changing card height

--- index.js
+++ index.js
@@ -84,7 +84,9 @@ $(document).ready(function(){
             <div class="card-body">
               <h5 class="card-title">${title}</h5>
               <p class="card-text">${description}</p>
-              <a href="${link}" class="btn btn-success btn-sm bshd">See</a>
+             </div>
+             <div class="card-footer">
+             <a href="${link}" class="btn btn-success btn-sm bshd">See</a>
              </div>
            </div>
          </div>`
